226f753bb2cb1979300d6579968036e53a9b8e42
Andrew James <ajames20@gmail.com>
2017-05-16 11:46:28 +0300

    add actions for adding/removing todos

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -8,12 +8,30 @@ let defaultState = {
   todos: []
 };
 
+let nextTodoID = 1;
+
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case 'CHANGE_SEARCH_TEXT':
       return {
         ...state,
         searchText: action.searchText
+      };
+    case 'ADD_TODO':
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            id: nextTodoID++,
+            todos: action.todo
+          }
+        ]
+      };
+    case 'REMOVE_TODO':
+      return {
+        ...state,
+        todos: state.todos.filter(todo => todo.id !== action.id)
       };
     default:
       return state;
@@ -31,6 +49,7 @@ let unsucribe = store.subscribe(() => {
   var state = store.getState();
   console.log('Name is: ', state.searchText);
   document.getElementById('app').innerHTML = state.searchText;
+  console.log('New State: ', store.getState());
 });
 // unsucribe(); allows you to unsuscribe from subscribe
 
@@ -40,11 +59,26 @@ store.dispatch({
 });
 
 store.dispatch({
-  type: 'CHANGE_SEARCH_TEXT',
-  searchText: 'Finding'
+  type: 'ADD_TODO',
+  todo: 'Running'
+});
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: 'Walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_TODO',
+  id: 2
 });
 
 store.dispatch({
   type: 'CHANGE_SEARCH_TEXT',
   searchText: 'Lost'
+});
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: 'Get Milk'
 });
